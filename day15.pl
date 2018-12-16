ee87e91937f890892a9aaa7e8f166f9a0103fea2
Keith Carangelo <keithcarangelo@safetyinsurance.com>
2018-12-16 11:04:08 -0500

    Finished day15 part a!!!!

--- day15.pl
+++ day15.pl
@@ -1,5 +1,13 @@
 #!/usr/bin/perl
 #
+# This was the hardest puzzle yet because of all of the specifications. I
+# resorted to looking at reddit and found several (python) solutions that
+# did not work with my input!
+#
+# I finally found one that did work and was able to detect my last error.
+# I should have been looking not for the path to the enemy, but the path
+# to the spot that put in enemy in *range*.
+#
 use strict;
 use warnings;
 
@@ -55,7 +63,7 @@ print "($unit->{ y }, $unit->{ x }) = $unit->{ hp }\n";
 
     my @targets = sort { $a->{ hp } <=> $b->{ hp } } map { $self->find_target( $enemy, $_->[0], $_->[1] ) } @target_pos;
     $targets[0]->{ hp } -= $unit->{ attack };
-print "($targets[0]->{ y }, $targets[0]->{ x }) : $targets[0]->{ hp }\n";
+#print "($targets[0]->{ y }, $targets[0]->{ x }) : $targets[0]->{ hp }\n";
     if ($targets[0]->{ hp } <= 0) {
       $self->kill_unit( $targets[0] );
      }
@@ -66,23 +74,36 @@ print "($targets[0]->{ y }, $targets[0]->{ x }) : $targets[0]->{ hp }\n";
   # https://www.reddit.com/r/adventofcode/comments/a6chwa/2018_day_15_solutions/ebu7u0z
   # and tested using test15z.txt
   #
+  # Also, I was checking the nearest *target*, but not the nearest *range*!
+  #
   sub move_unit {
     my ($self, $unit) = @_;
 
     my $enemy = $unit->{ type } eq 'E' ? 'G' : 'E';
     my $map = $self->{ map };
 
+    # Make sure there are still targets
+    if ($enemy eq 'E') {
+      $self->end_battle( 'G' ) unless (grep { $_->{ hp } > 0 } @{ $self->{ elves } });
+     }
+    else {
+      $self->end_battle( 'E' ) unless (grep { $_->{ hp } > 0 } @{ $self->{ goblins } });
+     }
+
+    # Test if we are already in range
+    return if (grep { $map->[$unit->{ y } + $_->[0]][$unit->{ x } + $_->[1]] eq $enemy } @moves);
+
     # First, we need to find the closest enemy
     my $possible = [ [ $unit->{ y }, $unit->{ x }, '' ] ];
     my %tested = ( "$unit->{ y },$unit->{ x }" => 1 );
-    my @closest_enemies = ();
-    while (!@closest_enemies && @{ $possible }) {
+    my @closest_range = ();
+    while (!@closest_range && @{ $possible }) {
       my $next = [];
       for my $p (@{ $possible }) {
         # Check if there is a target in range
         for my $m (@moves) {
           if ($map->[$p->[0] + $m->[0]][$p->[1] + $m->[1]] eq $enemy) {
-            push @closest_enemies, [$p->[0] + $m->[0], $p->[1] + $m->[1]];
+            push @closest_range, [$p->[0], $p->[1]];
            }
          }
 
@@ -102,41 +123,39 @@ print "($targets[0]->{ y }, $targets[0]->{ x }) : $targets[0]->{ hp }\n";
      }
 
     # Now, find the best way to move to that enemy
-    return unless (@closest_enemies);
-    @closest_enemies = sort { $a->[0] <=> $b->[0] || $a->[1] <=> $b->[1] } @closest_enemies;
-    my $closest = $closest_enemies[0];
+    return unless (@closest_range);
+    @closest_range = sort { $a->[0] <=> $b->[0] || $a->[1] <=> $b->[1] } @closest_range;
+    my $closest = $closest_range[0];
     $possible = [ [ $unit->{ y }, $unit->{ x }, '' ] ];
     %tested = ( "$unit->{ y },$unit->{ x }" => 1 );
     for my $p (@{ $possible }) {
-      # Check if the target is in range
-      if (grep { $p->[0] + $_->[0] == $closest->[0] && $p->[1] + $_->[1] == $closest->[1] } @moves) {
-        # Done
-        if ($p->[2]) {
-          $map->[ $unit->{ y } ][ $unit->{ x } ] = '.';
-          $unit->{ y } = $p->[2][0];
-          $unit->{ x } = $p->[2][1];
-          $map->[ $unit->{ y } ][ $unit->{ x } ] = $unit->{ type };
-         }
-        return;
-       }
-      
       # Move
       for my $m (@moves) {
         my $y = $p->[0] + $m->[0];
         my $x = $p->[1] + $m->[1];
-         next if $tested{ "$y,$x" };
-         next if $map->[$y][$x] ne '.';
-         $tested{ "$y,$x" } = 1;
-         my $first = $p->[2] || [ $y, $x ];
-         push @{ $possible }, [ $y, $x, $first ];
-        }
-      }
+        my $first = $p->[2] || [ $y, $x ];
+        if ($y == $closest->[0] && $x == $closest->[1]) {
+          # Done
+          $map->[ $unit->{ y } ][ $unit->{ x } ] = '.';
+          $unit->{ y } = $first->[0];
+          $unit->{ x } = $first->[1];
+          $map->[ $unit->{ y } ][ $unit->{ x } ] = $unit->{ type };
+          return;
+         }
+        next if $tested{ "$y,$x" };
+        next if $map->[$y][$x] ne '.';
+        $tested{ "$y,$x" } = 1;
+        push @{ $possible }, [ $y, $x, $first ];
+       }
+     }
 
-    return;
+    die "Error: unable to find path to target";
    }
 
   sub print_map {
     my ($self) = @_;
+
+    print "$self->{ num_rounds }\n";
     for my $row (@{ $self->{ map } }) {
       print join( '', @{ $row } ), "\n";
      }
@@ -147,17 +166,15 @@ print "($targets[0]->{ y }, $targets[0]->{ x }) : $targets[0]->{ hp }\n";
   sub round {
     my ($self) = @_;
 
+    # Remove dead units
+    $self->{ elves } = [ grep { $_->{ hp } > 0 } @{ $self->{ elves } } ];
+    $self->{ goblins } = [ grep { $_->{ hp } > 0 } @{ $self->{ goblins } } ];
+
     for my $unit (sort { $a->{ y } <=> $b->{ y } || $a->{ x } <=> $b->{ x } } (@{ $self->{ elves } }, @{ $self->{ goblins } })) {
       next unless ($unit->{ hp } > 0);
       $self->move_unit( $unit );
       $self->unit_attack( $unit );
      }
-
-    # Remove dead units
-    $self->{ elves } = [ grep { $_->{ hp } > 0 } @{ $self->{ elves } } ];
-    $self->end_battle( 'G' ) unless (@{ $self->{ elves } });
-    $self->{ goblins } = [ grep { $_->{ hp } > 0 } @{ $self->{ goblins } } ];
-    $self->end_battle( 'E' ) unless (@{ $self->{ goblins } });
 
     $self->{ num_rounds }++;
 
@@ -214,8 +231,8 @@ print "($targets[0]->{ y }, $targets[0]->{ x }) : $targets[0]->{ hp }\n";
 my $input_file = $ARGV[0] || 'input15.txt';
 
 my $map = Map->new( $input_file );
+$map->print_map();
 while (1) {
-  print "Round $map->{ num_rounds }\n";
   $map->round();
   $map->print_map();
  }
