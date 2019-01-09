9246f9ef0a82a80fb28c1bd68dd6e55fbbc36dca
Keith Carangelo <keithcarangelo@safetyinsurance.com>
2019-01-09 09:00:19 -0500

    Day 20a - Passes tests but not input

--- day20.pl
+++ day20.pl
@@ -1,54 +1,68 @@
 #!/usr/bin/perl
 #
+# First guess: 3362, which was too low
+#
 use strict;
 use warnings;
 
 use Path::Tiny;
 
-{ package Regex;
+{ package Map;
 
-  my %doors = ( 'N' => 0, 'E' => '1', 'S' => '2', 'W' => '3' );
   my %dirs = ( 'N' => [ -1, 0 ], 'E' => [ 0, 1 ], 'S' => [ 1, 0 ], 'W' => [ 0, -1 ] );
+
+  sub traverse {
+    my ($self, $curr) = @_;
+
+    return unless @{ $self->{ path } };
+
+    my $char = shift @{ $self->{ path } };
+    
+    if ($char eq '(') {
+      push @{ $curr->{ children } }, Path->new( $curr );
+      $curr = $curr->{ children }[0];
+     }
+    elsif ($char eq '|') {
+      $curr = $curr->{ parent };
+      push @{ $curr->{ children } }, Path->new( $curr );
+      $curr = $curr->{ children }[-1];
+     }
+    elsif ($char eq ')') {
+      $curr = $curr->{ parent };
+     }
+    else {
+      $curr->{ str } .= $char;
+      $self->{ map }{ "$curr->{ y },$curr->{ x }" }{ dirs } .= $char;
+      $curr->{ count }++;
+      $curr->{ y } += $dirs{ $char }->[0];
+      $curr->{ x } += $dirs{ $char }->[1];
+      if (!$self->{ map }{ "$curr->{ y },$curr->{ x }" }) {
+        $self->{ map }{ "$curr->{ y },$curr->{ x }" } = { dirs => '', count => $curr->{ count } };
+       }
+     }
+
+    return $self->traverse( $curr );
+   }
+
   sub new {
     my ($class, $input_file) = @_;
     my $self = {
       regex => Path->new(),
       map => {},
+      path => [],
      };
+    bless $self, $class;
 
     my $data = Path::Tiny::path( $input_file )->slurp_utf8();
     $data =~ s/^\^//;
     $data =~ s/\$(.*)$//sm;
+    $self->{ path } = [ split '', $data ];
 
     my $curr = $self->{ regex };
-    $self->{ map }{ "0,0" } = { dirs =>[ 0, 0, 0, 0 ], count => 0 };
+    $self->{ map }{ "0,0" } = { dirs => '', count => 0 };
 
-    for my $char (split '', $data) {
-      if ($char eq '(') {
-        push @{ $curr->{ children } }, Path->new( $curr );
-        $curr = $curr->{ children }[0];
-       }
-      elsif ($char eq '|') {
-        $curr = $curr->{ parent };
-        push @{ $curr->{ children } }, Path->new( $curr );
-        $curr = $curr->{ children }[-1];
-       }
-      elsif ($char eq ')') {
-        $curr = $curr->{ parent };
-       }
-      else {
-        $curr->{ str } .= $char;
-        $self->{ map }{ "$curr->{ y },$curr->{ x }" }{ dirs }[ $doors{ $char } ] = 1;
-        $curr->{ count }++;
-        $curr->{ y } += $dirs{ $char }->[0];
-        $curr->{ x } += $dirs{ $char }->[1];
-        if (!$self->{ map }{ "$curr->{ y },$curr->{ x }" }) {
-          $self->{ map }{ "$curr->{ y },$curr->{ x }" } = { dirs =>[ 0, 0, 0, 0 ], count => $curr->{ count } };
-         }
-       }
-     }
+    $self->traverse( $curr );
 
-    bless $self, $class;
     return $self;
    }
     
@@ -65,7 +79,6 @@ use Path::Tiny;
       children => [],
       y => 0,
       x => 0,
-      dirs => [ 0, 0, 0, 0 ],
       count => 0,
      };
 
@@ -82,11 +95,11 @@ use Path::Tiny;
 
 my $input_file = $ARGV[0] || 'input20.txt';
 
-my $regex = Regex->new( $input_file );
+my $map = Map->new( $input_file );
 
 my $max = 0;
-for my $room (keys %{ $regex->{ map } }) {
-  $max = $regex->{ map }{ $room }{ count } if ($max < $regex->{ map }{ $room }{ count });
+for my $room (keys %{ $map->{ map } }) {
+  $max = $map->{ map }{ $room }{ count } if ($max < $map->{ map }{ $room }{ count });
  }
 
 print "The farthest room is $max doors away\n";
